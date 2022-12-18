bdfdbf57edce55595c88bb09dee48d63a38b4765
Jhean Khendrick Galope <drakekhendrick@gmail.com>
2022-12-18 13:55:43 +0800

    simple-js-mobile

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-let cvc = document.getElementById('card-cvc');
-let inputCvc = document.getElementById('cvc');
-const confirm = document.querySelectorAll('button');
+const cvc = document.getElementById('card-cvc');
+const inputCvc = document.getElementById('cvc');
+const name = document.getElementById('card-name');
+const inputName = document.getElementById('name');
+const cardNumber = document.getElementById('card-number');
+const inputCN = document.getElementById('number');
+const month = document.getElementById('card-expiry');
+const inputMonth = document.getElementById('month');
+const year = document.getElementById('card-expiry1');
+const inputYear = document.getElementById('year');
+inputCvc.onkeyup = function(){
+    cvc.innerHTML = this.value;
+}
 
+inputName.onkeyup = function(){
+    name.innerHTML = this.value;
+}
 
-confirm.onclick = cvc.innerHTML = inputCvc.value;
+inputCN.onkeyup = function(){
+    cardNumber.innerHTML = this.value;
+}
+
+inputMonth.onkeyup = function(){
+    month.innerHTML = `${this.value}/`
+    if(month.innerHTML === ' '){
+        month.innerHTML = '00/'
+    }
+}
+
+inputYear.onkeyup = function(){
+    year.innerHTML = this.value;
+}
