b92450c39b1f919dc3d656991913e437a77cf27d
Jhean Khendrick Galope <drakekhendrick@gmail.com>
2022-12-18 13:05:03 +0800

    mobile-display

---
+++ script.js
@@ -0,0 +1,6 @@
+let cvc = document.getElementById('card-cvc');
+let inputCvc = document.getElementById('cvc');
+const confirm = document.querySelectorAll('button');
+
+
+confirm.onclick = cvc.innerHTML = inputCvc.value;
