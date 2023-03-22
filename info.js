c30335fd79fd5fd78fbc28be356901f5aea69c32
Jhean <drakekhendrick@gmail.com>
2023-03-22 22:56:26 +0800

    rename js file

---
+++ info.js
@@ -0,0 +1,42 @@
+
+const cvc = document.getElementById('card-cvc');
+const inputCvc = document.getElementById('cvc');
+const name = document.getElementById('card-name');
+const inputName = document.getElementById('name');
+const cardNumber = document.getElementById('card-number');
+const inputCN = document.getElementById('number');
+const month = document.getElementById('card-expiry');
+const inputMonth = document.getElementById('month');
+const year = document.getElementById('card-expiry1');
+const inputYear = document.getElementById('year');
+inputCvc.onkeyup = function () {
+    cvc.innerHTML = this.value;
+}
+
+inputName.onkeyup = function () {
+    name.innerHTML = this.value;
+}
+
+inputCN.onkeyup = function(){
+    cardNumber.textContent = inputCN.value
+};
+
+const array = [];
+
+inputCN.addEventListener('input', (e) => {
+    inputCN.value = inputCN.value.replace(/\D/g, '')
+    .replace(/(\d{4})/g, '$1 ');
+    console.log(inputCN.value);
+})
+  
+
+inputMonth.onkeyup = function () {
+    month.innerHTML = `${this.value}/`
+    if (month.innerHTML === ' ') {
+        month.innerHTML = '00/'
+    }
+}
+
+inputYear.onkeyup = function () {
+    year.innerHTML = this.value;
+}
