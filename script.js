85aaf9b8f0fc46b57998fbe384b51b7077708989
Jhean <drakekhendrick@gmail.com>
2023-03-22 21:52:27 +0800

    tested a few changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const cvc = document.getElementById('card-cvc');
 const inputCvc = document.getElementById('cvc');
 const name = document.getElementById('card-name');
@@ -8,25 +9,28 @@ const month = document.getElementById('card-expiry');
 const inputMonth = document.getElementById('month');
 const year = document.getElementById('card-expiry1');
 const inputYear = document.getElementById('year');
-inputCvc.onkeyup = function(){
+inputCvc.onkeyup = function () {
     cvc.innerHTML = this.value;
 }
 
-inputName.onkeyup = function(){
+inputName.onkeyup = function () {
     name.innerHTML = this.value;
 }
 
-inputCN.onkeyup = function(){
-    cardNumber.innerHTML = this.value;
-}
+inputCN.addEventListener('keyup', cardNumber.textContent = this.value);
 
-inputMonth.onkeyup = function(){
+inputCN.addEventListener('input', (e) => {
+    ccNum.value = ccNum.value.replace(/\D/g, '')
+    .replace(/^(.{4})(.{4})(.{4})(.*)$/, '$1 $2 $3 $4');
+})
+
+inputMonth.onkeyup = function () {
     month.innerHTML = `${this.value}/`
-    if(month.innerHTML === ' '){
+    if (month.innerHTML === ' ') {
         month.innerHTML = '00/'
     }
 }
 
-inputYear.onkeyup = function(){
+inputYear.onkeyup = function () {
     year.innerHTML = this.value;
 }
